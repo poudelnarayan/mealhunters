5def64182ebe228e177608d38ed3a690e46cefa9
Narayan Poudel <poudelnarayan434@gmail.com>
2025-07-20 02:13:31 +0545

    outputting meals data and iamges with unknown dimension

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meal-grid";
+
 export default function MealsPage() {
-  return <h1> All meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals , created
+          <span className={classes.highlight}> by you </span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself . It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recpie</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
