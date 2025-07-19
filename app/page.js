c7d1359b2f8b748f43c50eca7b6f0a97f0b4c0a2
Narayan Poudel <poudelnarayan434@gmail.com>
2025-07-20 01:21:37 +0545

    preparing an image slideshow

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import classes from './page.module.css';
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
@@ -23,8 +26,8 @@ export default function Home() {
           <h2>How it works</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
@@ -36,8 +39,8 @@ export default function Home() {
           <h2>Why NextLevel Food?</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
