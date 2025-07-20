3213ee3335c5024191f9802e404fb2dfb56d22dc
Narayan Poudel <poudelnarayan434@gmail.com>
2025-07-21 00:29:10 +0545

    adding dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,17 @@ import classes from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailaPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
